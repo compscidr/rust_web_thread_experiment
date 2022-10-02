4876cc2796de58aaa3234eea05ab04f67aa3d21e
Jason Ernst <ernstjason1@gmail.com>
2022-10-02 12:26:29 -0700

    working PoC

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # rust-web--thread-experiment
+The main thing I want to accomplish with this repo is to have some type of queue which can have more items added to it
+from the main thread (or any other thread), while the web server is able to serve the state of the queue (size,
+contents, etc.)
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,59 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+use std::ops::Deref;
+use std::sync::Arc;
+use std::thread::sleep;
+use actix_web::{get, web, rt, App, HttpServer, Responder};
+use crossbeam_queue::ArrayQueue;
+
+struct MyData {
+    queue: ArrayQueue<i32>,
+}
+
+fn manip_queue(data: &MyData)  {
+    loop {
+        let random_number = rand::random::<i32>();
+        println!("Pushing {} to queue", random_number);
+        let _ = data.queue.push(random_number);
+        sleep(std::time::Duration::from_millis(1000));
+    }
+}
+
+#[get("/")]
+async fn display_queue(data: web::Data<MyData>) -> String {
+    println!("Displaying queue");
+    let mut result = String::new();
+    for i in 0..data.queue.len() {
+        result.push_str(&data.queue.pop().unwrap().to_string());
+        result.push_str(" ");
+    }
+    result
+}
 
 #[get("/hello/{name}")]
 async fn greet(name: web::Path<String>) -> impl Responder {
     format!("Hello {name}!")
 }
 
+
+// todo: perhaps don't use the actix_web::main: https://github.com/actix/actix-web/issues/1283
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+
+    let data = web::Data::new(MyData {
+        queue: ArrayQueue::new(10),
+    });
+
+    std::thread::spawn( {
+        let inner_config = Arc::clone(&data);
+        move || {
+            manip_queue(inner_config.as_ref().deref());
+        }
+    });
+
+    HttpServer::new(move || {
         App::new()
-            .route("/hello", web::get().to(|| async { "Hello World!" }))
+            .app_data(data.clone())
             .service(greet)
+            .service(display_queue)
     })
         .bind(("127.0.0.1", 8080))?
         .run()
